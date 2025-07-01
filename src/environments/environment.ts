7da56257b15300f1ae4e393a1e21a6d91ed44b03
Fabian Mardones <fabianmardones94@gmail.com>
2025-07-01 16:00:39 -0400

    Integración firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCi-Pb2xo8BQHM_YoVo_dgjP83HwVxjyaA",
+    authDomain: "getabestchoise.firebaseapp.com",
+    projectId: "getabestchoise",
+    storageBucket: "getabestchoise.firebasestorage.app",
+    messagingSenderId: "703887195958",
+    appId: "1:703887195958:web:f2498f796261e888543d97",
+    measurementId: "G-H7LDQS9D18"
+  }
 };
 
 /*
